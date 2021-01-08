5a6a9739b281b3f783fee65ec895125f5e2a2c23
shubham <xaocofachlys@gmail.com>
2021-01-08 14:47:58 +0530

    Question 2 of lab mannual

---
+++ q1.c
@@ -0,0 +1,16 @@
+/* Write a program to display message in the following manner using \n and \n if applicable.
+  a)HelloWorld
+  b)Hello World
+  c)Hello
+    World
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+    printf("HelloWorld\n");
+    printf("Hello World\n");
+    printf("Hello\nWorld\n");
+return 0;
+//@xaocofachlys
+}
